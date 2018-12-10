28544c7f5dc58d12c39e580c3226bfd698167268
Patdanai <patdanai.duangwaropas@gmail.com>
2018-12-10 21:24:50 +0700

    add preprocessed datasets

--- ga_model.py
+++ ga_model.py
@@ -1,11 +1,11 @@
 import numpy as np
-np.random.seed(2)
+# np.random.seed(2)
 
 # solution = [w1, w2, ..., wn]
 class Individual(object): # create individual
     def __init__(self, w_shape):
         self.solution = np.random.randn(w_shape[0], w_shape[1])
-        self.fitness = 0
+        self.fitness = -(np.inf)
 
 # train(x) -> cal_error(y_hat) -> fitness or cost(y_hat) -> update_with_ga(y_hat) -> train
 
@@ -47,63 +47,86 @@ def forward_pass(d, form, population, x):
 
 def train(epochs, form, d, population, x): # epoch is generation
     j = 0
+    optimal_solution_index = 0
+    optimal_fitness = -(np.inf)
+    optimal_solution = population[0]
+    
     while(j < epochs):
-        print(population)
         # update fitness
+        # print(j)
+        # for i in range(len(population)):
+        #     print(population[i].solution)
+        #     print(population[i].fitness)
+        # print('before')
+        # for i in range(len(population)):
+            # print(population[i].solution)
+            # print(population[i].fitness)
+
         y_hats = forward_pass(d, form, population, x)
         for i in range(len(population)):
             population[i].fitness = calculate_fitness(d, population[i], y_hats[i])
-            print(population[i].fitness)
         # sort population by fitness
         population = np.asarray(sort_by_fitness(population))
 
         ###
         # ga process goes below
         # ---------------------------------------------
-        winner_pool, winner_index = tournament_selection(d, (len(population) / 2) + 1, population, len(population))
-        print(winner_pool, winner_index)
+        winner_pool, winner_index = tournament_selection(d, epochs, ((len(population) / 2) + 1), population, len(population))
+        # print('***', winner_pool[0].solution)
+        # for i in range(len(winner_pool)):
+            # print(winner_pool[i].solution)
+        mate(winner_pool, 1 / len(population))
+        # print()
+        # for i in range(len(winner_pool)):
+            # print(winner_pool[i].solution)
+        elite_index = len(population) - int((len(population) / 2) + 1)
+        k = 0
+        for i in range(elite_index, len(population)):
+            # print(population[i].solution)
+            population[i].solution = winner_pool[k].solution
+            k += 1
+            # print(population[i].solution)
+        # print(list(set(winner_pool)), list(set(winner_index)))
+        # print(winner_pool[0].solution)
         # mate(winner_pool, (1 / len(population)))
         ###
 
-        optimal_solution_index = 0
-        optimal_fitness = population[0].fitness
-        optimal_solution = population[0]
+        # print('after')
+        # for i in range(len(population)):
+            # print(population[i].solution)
+            # print(population[i].fitness)
+        
         for i in range(1, len(population)):
             if(population[i].fitness > optimal_fitness):
                 optimal_solution_index = i
                 optimal_fitness = population[i].fitness
                 optimal_solution = population[i]
-        print('fitness:', optimal_fitness)
-        print(('individual[' + str(optimal_solution_index) + ']:'), form_network(form, optimal_solution))
+        if(j % 128 == 0):
+            print('fitness:', population[0].fitness)
+        # print(('individual[' + str(optimal_solution_index) + ']:'), form_network(form, optimal_solution))
         j += 1
+    print(optimal_fitness)
+    print(optimal_solution_index)
+    print(optimal_solution.solution)
+    return optimal_solution
 
 def mate(mating_pool, mutation_rate):
     try:
         for i in range(0, len(mating_pool), 2):
-            # print('before')
-            # print(mating_pool[i].solution)
-            # print(mating_pool[i+1].solution)
             cross_over(mating_pool[i], mating_pool[i+1])
-            # print('after')
-            # print(mating_pool[i].solution)
-            # print(mating_pool[i+1].solution)
-    except IndexError: # if there's odd number of mating pool member mutate the last one that has no pair
-        # print('before mutation')
-        # print(mating_pool[-1].solution)
-        mutate(mutation_rate, mating_pool[-1], individual=True)
-        # print('after mutation')
-        # print(mating_pool[-1].solution)
 
-def tournament_selection(d, n, population, population_size): # n is desired number of offsprings
+    except IndexError: # if there's odd number of mating pool member mutate the last one that has no pair
+        mutate(mutation_rate, mating_pool[-1], individual=True)
+
+def tournament_selection(d, iteration, n, population, population_size): # n is desired number of offsprings
     n = int(n)
-    p = 1 / population_size
+    p = .5 * np.exp(-.5 * iteration)
     winner_pool = []
     winner_indexes = []
     i = 0
     while(i < n):
         index1 = np.random.randint(population_size)
         index2 = np.random.randint(population_size)
-        print((index1, index2))
         while(index1 == index2):
             index2 = np.random.randint(population_size)
         r = np.random.random()
@@ -112,11 +135,17 @@ def tournament_selection(d, n, population, population_size): # n is desired numb
             if(r < p):
                 winner_pool.append(population[index2])
                 winner_indexes.append(index2)
+                # print((index2, index1))
             else:
                 winner_pool.append(population[index1])
                 winner_indexes.append(index1)
+                # print((index1, index2))
+        else:
+            winner_pool.append(population[index2])
+            winner_indexes.append(index2)
+            # print((index2, index1))
         i += 1
-    print('wi', winner_indexes)
+    # print('wi', winner_indexes)
     return winner_pool, winner_indexes
 
 def cross_over(i1, i2): # mating of individual1 and individual2
@@ -158,22 +187,30 @@ def calculate_fitness(d, population, y_hat):
     return -cross_entropy(y_hat, d)
 
 if(__name__ == '__main__'):
-    iterations = 2048
+    iterations = 1000
     population = []
 
-    size = 10
+    size = 100
     mutation_rate = 1 / size
 
-    x = np.random.randn(10, 5)
+    wdbc_data = np.genfromtxt('wdbc-norm.csv', delimiter=',')
+    x = np.copy(wdbc_data).T
+    x = x[1:].T
+    print(x)
+    d = (np.copy(wdbc_data)).T
+    d = d[0].reshape(d[0].shape[0], 1)
+    # print(d)
+
+    # x = np.random.randn(10, 3)
     # print(x)
-    d = np.random.randint(2, size=(10, 2))
+    # d = np.random.randint(2, size=(10, 2))
     # print(d)
 
     for i in range(size):
-        population.append(Individual((25, 1))) # 5 x 3 + 3 x 2
+        population.append(Individual((90, 1))) # 5 x 3 + 3 x 2
     population = np.asarray(population)
 
-    mate(population, mutation_rate)
-    mutate(mutation_rate, population)
+    # mate(population, mutation_rate)
+    # mutate(mutation_rate, population)
     # forward_pass(d, '5, 3, 2, 2', population, x)
-    train(3, '5, 3, 2, 2', d, population, x)
+    optimal_solution = train(iterations, '30, 3', d, population, x)
